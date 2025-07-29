9272a7525602dfdee20a3b7fb8a35ff7b0a36ed1
Marco Platino <mistermarcop@hotmail.com>
2025-07-29 13:30:10 -0400

    I renamed the primegeneration.py file to primes.py added fifugurenumbers.py updated pyproject.toml added new build

--- src/EulerKit/primes.py
+++ src/EulerKit/primes.py
@@ -30,3 +30,11 @@ def makePrimeFile(x:int, y:int, path:str = "/home/marco/1MillionPrimes.txt") -> 
             f.write(f"{i}\n")
     
     return path
+
+def isPrime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int((number ** 0.5))):
+        if number % i == 0:
+            return False
+    return True
