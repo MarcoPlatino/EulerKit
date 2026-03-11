36316ae0221c282597e90a0545c0db701bbc458c
Marco Platino <155002190+MarcoPlatino@users.noreply.github.com>
2026-03-11 15:15:51 -0400

    Update and rename equivalency.py to anagrams.py

--- src/EulerKit/anagrams.py
+++ src/EulerKit/anagrams.py
@@ -4,12 +4,17 @@ def containSameDigits(x, y):
     xList = lt.numberToList(x)
     yList = lt.numberToList(y)
 
+
+    
     if not len(xList) == len(yList):
         return False
-    
+
+    xFreq = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for i in xList:
-        if i in yList:
-            continue
-        return False
-    
-    return True
+        xFreq[i] += 1
+        
+    yFreq = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in yList:
+        yFreq[i] += 1
+
+    return xFreq == yFreq
