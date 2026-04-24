cdeb0a9f0c9453853b3e35dc86ceab37f15670fc
Marco Platino <mistermarcop@hotmail.com>
2026-04-24 18:02:15 -0400

    Added a feature to find the next permutation of a given sequence of numbers. This is useful for generating all possible permutations in a systematic way. The implementation uses a well-known algorithm that involves finding the rightmost pair of indices where the first index is less than the second, swapping the values at these indices, and then reversing the order of the elements to the right of the first index. This ensures that we get the next lexicographical permutation.

---
+++ src/EulerKit/__init__.py
@@ -0,0 +1,7 @@
+# import anagrams
+# import listtools
+
+# anagram = anagrams.containSameDigits
+# lt = listtools
+
+# __all__ = [listtools, lt, anagram, anagrams]
--- src/EulerKit/permutations.py
+++ src/EulerKit/permutations.py
@@ -1,6 +1,19 @@
 import math
 import listtools as lt
 
+def generatePermutations(n): #This can only be used with smaller data sets...
+    perms = []
+    def permHelper(end, start = ""):
+        if (len(end) <= 1):
+            perms.append(start + end)
+        else:
+            for i in range(len(end)):
+                if (end[i:i+1] in end[0:i]):
+                    continue
+                permHelper(end[0:i] + end[i+1:], start + end[i:i+1])
+
+    permHelper(n)
+    return perms
 
     
 
@@ -17,4 +30,4 @@ def permutations_recursive(n:list):
 
         
 if __name__ == "__main__":
-    # permutations_basic(123)
+    print(generatePermutations("123456789"))
